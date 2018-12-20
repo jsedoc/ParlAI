9344999b299f4e094711446048b5ebabb4bb0ee5
Caroline Okun <carokun@tesla.ldc.upenn.edu>
2018-12-20 18:33:40 -0500

    adding code for tmdb labels

--- parlai/agents/caed/caed.py
+++ parlai/agents/caed/caed.py
@@ -9,32 +9,34 @@
 http://parl.ai/static/docs/tutorial_seq2seq.html
 """
 
-from parlai.core.torch_agent import TorchAgent, Output
-from .modules import EncoderRNN, DecoderRNN
+#from parlai.core.torch_agent import TorchAgent, Output
+from parlai.core.torch_generator_agent import TorchGeneratorAgent
+from .modules import Seq2seq, opt_to_kwargs
 
 import torch
-from torch import optim
 import torch.nn as nn
-import torch.nn.functional as F
+
 import spacy, tqdm
 import collections
 from collections import defaultdict
 import concurrent.futures
+import requests, json
+import zlib
+import datetime
 # from ccg_nlpy import remote_pipeline
 
 
-class CaedAgent(TorchAgent):
+class CaedAgent(TorchGeneratorAgent):
     """Agent which takes an input sequence and produces an output sequence.
 
     This model is based on Sean Robertson's `seq2seq tutorial
     <http://pytorch.org/tutorials/intermediate/seq2seq_translation_tutorial.html>`_.
     """
 
-    @staticmethod
-    def add_cmdline_args(argparser):
+    @classmethod
+    def add_cmdline_args(cls, argparser):
         """Add command-line arguments specifically for this agent."""
-        TorchAgent.add_cmdline_args(argparser)
-        agent = argparser.add_argument_group('Seq2Seq Arguments')
+        agent = argparser.add_argument_group('CAED Seq2Seq Arguments')
         agent.add_argument('-hs', '--hiddensize', type=int, default=128,
                            help='size of the hidden layers')
         agent.add_argument('-esz', '--embeddingsize', type=int, default=128,
@@ -45,12 +47,57 @@ class CaedAgent(TorchAgent):
                            help='learning rate')
         agent.add_argument('-dr', '--dropout', type=float, default=0.1,
                            help='dropout rate')
-        agent.add_argument('--gpu', type=int, default=-1,
-                           help='which GPU device to use')
-        agent.add_argument('-rf', '--report-freq', type=float, default=0.001,
-                           help='Report frequency of prediction during eval.')
+        agent.add_argument('-bi', '--bidirectional', type='bool',
+                           default=False,
+                           help='whether to encode the context with a '
+                                'bidirectional rnn')
+        agent.add_argument('-att', '--attention', default='none',
+                           choices=['none', 'concat', 'general', 'dot',
+                                    'local'],
+                           help='Choices: none, concat, general, local. '
+                                'If set local, also set attention-length. '
+                                '(see arxiv.org/abs/1508.04025)')
+        agent.add_argument('-attl', '--attention-length', default=48, type=int,
+                           help='Length of local attention.')
+        agent.add_argument('--attention-time', default='post',
+                           choices=['pre', 'post'],
+                           help='Whether to apply attention before or after '
+                                'decoding.')
+        agent.add_argument('-rnn', '--rnn-class', default='lstm',
+                           choices=Seq2seq.RNN_OPTS.keys(),
+                           help='Choose between different types of RNNs.')
+        agent.add_argument('-dec', '--decoder', default='same',
+                           choices=['same', 'shared'],
+                           help='Choose between different decoder modules. '
+                                'Default "same" uses same class as encoder, '
+                                'while "shared" also uses the same weights. '
+                                'Note that shared disabled some encoder '
+                                'options--in particular, bidirectionality.')
+        agent.add_argument('-lt', '--lookuptable', default='unique',
+                           choices=['unique', 'enc_dec', 'dec_out', 'all'],
+                           help='The encoder, decoder, and output modules can '
+                                'share weights, or not. '
+                                'Unique has independent embeddings for each. '
+                                'Enc_dec shares the embedding for the encoder '
+                                'and decoder. '
+                                'Dec_out shares decoder embedding and output '
+                                'weights. '
+                                'All shares all three weights.')
+        agent.add_argument('-soft', '--numsoftmax', default=1, type=int,
+                           help='default 1, if greater then uses mixture of '
+                                'softmax (see arxiv.org/abs/1711.03953).')
+        agent.add_argument('-idr', '--input-dropout', type=float, default=0.0,
+                           help='Probability of replacing tokens with UNK in training.')
+        super(cls, CaedAgent).add_cmdline_args(argparser)
         CaedAgent.dictionary_class().add_cmdline_args(argparser)
         return agent
+
+    @staticmethod
+    def model_version():
+        """Return current version of this model, counting up from 0.
+
+        """
+        return 0
 
     def __init__(self, opt, shared=None):
         """Initialize example seq2seq agent.
@@ -59,136 +106,112 @@ class CaedAgent(TorchAgent):
         :param shared: optional shared dict with preinitialized model params
         """
         super().__init__(opt, shared)
+        self.id = 'Caed'
+        # store latest tmdb data
+        self.tmdb_ent_dict = self.get_tmdb_ids()
 
-        self.id = 'Seq2Seq'
-        self.nlp = spacy.load('en_core_web_lg')
+    def build_model(self, states=None):
+        """Initialize model, override to change model setup."""
+        opt = self.opt
+        if not states:
+            states = {}
 
-        if not shared:
-            # set up model from scratch
-            hsz = opt['hiddensize']
-            nl = opt['numlayers']
+        kwargs = opt_to_kwargs(opt)
+        self.model = Seq2seq(
+            len(self.dict), opt['embeddingsize'], opt['hiddensize'],
+            padding_idx=self.NULL_IDX, start_idx=self.START_IDX,
+            end_idx=self.END_IDX, unknown_idx=self.dict[self.dict.unk_token],
+            longest_label=states.get('longest_label', 1),
+            **kwargs)
 
-            # encoder captures the input text
-            self.encoder = EncoderRNN(len(self.dict), hsz, nl)
-            # decoder produces our output states
-            self.decoder = DecoderRNN(len(self.dict), hsz, nl)
+        if (opt.get('dict_tokenizer') == 'bpe' and
+                opt['embedding_type'] != 'random'):
+            print('skipping preinitialization of embeddings for bpe')
+        elif not states and opt['embedding_type'] != 'random':
+            # `not states`: only set up embeddings if not loading model
+            self._copy_embeddings(self.model.decoder.lt.weight,
+                                  opt['embedding_type'])
+            if opt['lookuptable'] in ['unique', 'dec_out']:
+                # also set encoder lt, since it's not shared
+                self._copy_embeddings(self.model.encoder.lt.weight,
+                                      opt['embedding_type'], log=False)
 
-            if self.use_cuda:  # set in parent class
-                self.encoder.cuda()
-                self.decoder.cuda()
+        if states:
+            # set loaded states if applicable
+            self.model.load_state_dict(states['model'])
 
-            if opt.get('numthreads', 1) > 1:
-                self.encoder.share_memory()
-                self.decoder.share_memory()
-        elif 'encoder' in shared:
-            # copy initialized data from shared table
-            self.encoder = shared['encoder']
-            self.decoder = shared['decoder']
-
-        # set up the criterion
-        self.criterion = nn.NLLLoss()
-
-        # set up optims for each module
-        lr = opt['learningrate']
-        self.optims = {
-            'encoder': optim.SGD(self.encoder.parameters(), lr=lr),
-            'decoder': optim.SGD(self.decoder.parameters(), lr=lr),
-        }
-
-        self.longest_label = 1
-        self.hiddensize = opt['hiddensize']
-        self.numlayers = opt['numlayers']
-        self.START = torch.LongTensor([self.START_IDX])
         if self.use_cuda:
-            self.START = self.START.cuda()
+            self.model.cuda()
 
-        self.reset()
+        if opt['embedding_type'].endswith('fixed'):
+            print('Seq2seq: fixing embedding weights.')
+            self.model.decoder.lt.weight.requires_grad = False
+            self.model.encoder.lt.weight.requires_grad = False
+            if opt['lookuptable'] in ['dec_out', 'all']:
+                self.model.decoder.e2s.weight.requires_grad = False
 
-    def zero_grad(self):
-        """Zero out optimizer."""
-        for optimizer in self.optims.values():
-            optimizer.zero_grad()
+        self.model.nlp = spacy.load('en_core_web_lg')
 
-    def update_params(self):
-        """Do one optimization step."""
-        for optimizer in self.optims.values():
-            optimizer.step()
 
-    def share(self):
-        """Share internal states."""
-        shared = super().share()
-        shared['encoder'] = self.encoder
-        shared['decoder'] = self.decoder
-        return shared
+        if self.use_cuda:
+            self.model.cuda()
+            if self.multigpu:
+                self.model = torch.nn.DataParallel(self.model)
+                self.model.encoder = self.model.module.encoder
+                self.model.decoder = self.model.module.decoder
+                self.model.longest_label = self.model.module.longest_label
+                self.model.output = self.model.module.output
+                self.model.reorder_encoder_states = (
+                    self.model.module.reorder_encoder_states
+                )
 
-    # only works if text is of the form "Can I see your AMA ?" with a space before punctuation
-    # def process_line(self, text):
-    #     target_ents = ["ORG", "PERSON"]
-    #     print('\n')
+        return self.model
 
-    #     print('ORIGINAL TEXT :' + text + '\n')
+    def get_tmdb_ids(self):
+        ent_dict = dict()
+        date = (datetime.datetime.now() - datetime.timedelta(1)).strftime('%m_%d_%Y')
+        person_input_file = 'http://files.tmdb.org/p/exports/person_ids_' + date + '.json.gz'
+        movie_input_file = 'http://files.tmdb.org/p/exports/movie_ids_' + date + '.json.gz'
+        r_person = requests.get(person_input_file)
+        r_movie = requests.get(movie_input_file)
 
-    #     l = text
+        decompressed_data = zlib.decompress(r_person.content, 16+zlib.MAX_WBITS)
+        decompressed_data = decompressed_data.decode("utf-8")
+        for ent in decompressed_data.split('\n'):
+            if ent != None and ent != '\n' and ent.strip() != '':
+                ent_dict[json.loads(ent)['name']] = 'PER'
 
-    #     word_to_ent = dict()
-    #     ent_counts = dict()
+        decompressed_data = zlib.decompress(r_movie.content, 16+zlib.MAX_WBITS)
+        decompressed_data = decompressed_data.decode("utf-8")
+        for ent in decompressed_data.split('\n'):
+            if ent != None and ent != '\n' and ent.strip() != '':
+                ent_dict[json.loads(ent)['original_title']] = 'WORK_OF_ART'
 
-    #     spdoc = self.nlp(l)
-                    
-    #     for e in spdoc.ents:
-    #         word_to_ent[e.as_doc().text.strip()] = e.label_
-
-    #     for word in word_to_ent:
-    #         #print(word + "..." + word_to_ent[word])
-    #         ent = word_to_ent[word]
-    #         previous_count = 0
-    #         if ent in ent_counts:
-    #             previous_count = ent_counts[ent]
-    #         ent_counts[ent] = previous_count + 1
-    #         word_to_ent[word] = word_to_ent[word] + '@' + str(ent_counts[ent])
-            
-    #     for word in word_to_ent:
-    #       if (word != ''):
-    #         text = text.replace(word, word_to_ent[word])
-        
-    #     print('CONVERTED TEXT :' + text + '\n')
-    #     return text
+        return ent_dict
 
     def process_line(self, text):
-    
+
         # first preprocess using database of movie labels
-      
+
         target_ents = ["ORG", "PERSON"]
 
         l = text
 
         if "__null__" in text:
-        	return ""
+        	return "__null__"
 
         print('\n')
         print('ORIGINAL TEXT :' + text + '\n')
 
         word_to_ent = dict()
         ent_counts = dict()
-        movie_labels = ['PER']
-        
-        # movie ents
-        # p = remote_pipeline.RemotePipeline()
 
-        # doc = p.doc(text)
-        
-        # if doc != None:
-        #   ner_view = doc.get_ner_conll
-        #   if ner_view.get_cons() != None:
-        #     for ner in ner_view:
-        #       print(ner['label'])
-        #       if ner['label'] in movie_labels:
-        #         word_to_ent[ner['tokens']] = ner['label']
-        
-        # end of movie ents
+        spdoc = self.model.nlp(l)
 
-        spdoc = self.nlp(l)
-                    
+        for word in self.tmdb_ent_dict:
+            if word in text:
+                word_to_ent[word] = self.tmdb_ent_dict[word]
+
         for e in spdoc.ents:
             #  word -> entity
             word = e.as_doc().text.strip()
@@ -202,17 +225,14 @@ class CaedAgent(TorchAgent):
                 previous_count = ent_counts[ent]
             ent_counts[ent] = previous_count + 1
             word_to_ent[word] = word_to_ent[word] + '@' + str(ent_counts[ent])
-            
+
         for word in word_to_ent:
-          if (word != ''):
+          if (word != '' and word not in self.tmdb_ent_dict):
             text = text.replace(word, word_to_ent[word])
-          
+
         print('CONVERTED TEXT :' + text + '\n')
         return text
 
-    def get_tmdb_ids(self):
-    	data = json.loads('http://files.tmdb.org/p/exports/movie_ids_04_28_2017.json.gz')
-        
 
     def v2t(self, vector):
         """Convert vector to text.
@@ -239,7 +259,7 @@ class CaedAgent(TorchAgent):
         """Call vectorize without adding start tokens to labels."""
         kwargs['add_start'] = False
 
-        # (Pdb) p args[0]['text'] 
+        # (Pdb) p args[0]['text']
         # 'your persona: i like to remodel homes.\nyour persona: i like to go hunting.\nyour persona: i like to shoot a bow.\nyour persona: my favorite holiday is halloween.\nhi , how are you doing ? i am getting ready to do some cheetah chasing to stay in shape .'
         # (Pdb) p args[0]['labels']
         # ('you must be very fast . hunting is one of my favorite hobbies .',)
@@ -247,86 +267,42 @@ class CaedAgent(TorchAgent):
 
         return super().vectorize(*args, **kwargs)
 
-    def train_step(self, batch):
-        """Train model to produce ys given xs.
+    def batchify(self, *args, **kwargs):
+        """Override batchify options for seq2seq."""
+        kwargs['sort'] = True  # need sorted for pack_padded
+        return super().batchify(*args, **kwargs)
 
-        :param batch: parlai.core.torch_agent.Batch, contains tensorized
-                      version of observations.
+    def save(self, path=None):
+        """Save model parameters if model_file is set."""
+        path = self.opt.get('model_file', None) if path is None else path
 
-        Return estimated responses, with teacher forcing on the input sequence
-        (list of strings of length batchsize).
-        """
-        xs, ys = batch.text_vec, batch.label_vec
-        if xs is None:
-            return
-        bsz = xs.size(0)
-        starts = self.START.expand(bsz, 1)  # expand to batch size
-        loss = 0
-        self.zero_grad()
-        self.encoder.train()
-        self.decoder.train()
-        target_length = ys.size(1)
-        # save largest seen label for later
-        self.longest_label = max(target_length, self.longest_label)
+        if path and hasattr(self, 'model'):
+            model = {}
+            if self.multigpu:
+                model['model'] = self.model.module.state_dict()
+            else:
+                model['model'] = self.model.state_dict()
+            model['longest_label'] = self.model.longest_label
+            model['optimizer'] = self.optimizer.state_dict()
+            model['optimizer_type'] = self.opt['optimizer']
 
-        _encoder_output, encoder_hidden = self.encoder(xs)
+            with open(path, 'wb') as write:
+                torch.save(model, write)
 
-        # Teacher forcing: Feed the target as the next input
-        y_in = ys.narrow(1, 0, ys.size(1) - 1)
-        decoder_input = torch.cat([starts, y_in], 1)
-        decoder_output, decoder_hidden = self.decoder(decoder_input,
-                                                      encoder_hidden)
+            # save opt file
+            with open(path + '.opt', 'w') as handle:
+                # save version string
+                self.opt['model_version'] = self.model_version()
+                json.dump(self.opt, handle)
 
-        scores = decoder_output.view(-1, decoder_output.size(-1))
-        loss = self.criterion(scores, ys.view(-1))
-        loss.backward()
-        self.update_params()
-
-        _max_score, predictions = decoder_output.max(2)
-        return Output(self.v2t(predictions))
-
-    def eval_step(self, batch):
-        """Generate a response to the input tokens.
-
-        :param batch: parlai.core.torch_agent.Batch, contains tensorized
-                      version of observations.
-
-        Return predicted responses (list of strings of length batchsize).
-        """
-        xs = batch.text_vec
-        if xs is None:
-            return
-        bsz = xs.size(0)
-        starts = self.START.expand(bsz, 1)  # expand to batch size
-        # just predict
-        self.encoder.eval()
-        self.decoder.eval()
-        _encoder_output, encoder_hidden = self.encoder(xs)
-
-        predictions = []
-        done = [False for _ in range(bsz)]
-        total_done = 0
-        decoder_input = starts
-        decoder_hidden = encoder_hidden
-
-        for _ in range(self.longest_label):
-            # generate at most longest_label tokens
-            decoder_output, decoder_hidden = self.decoder(decoder_input,
-                                                          decoder_hidden)
-            _max_score, preds = decoder_output.max(2)
-            predictions.append(preds)
-            decoder_input = preds  # set input to next step
-
-            # check if we've produced the end token
-            for b in range(bsz):
-                if not done[b]:
-                    # only add more tokens for examples that aren't done
-                    if preds[b].item() == self.END_IDX:
-                        # if we produced END, we're done
-                        done[b] = True
-                        total_done += 1
-            if total_done == bsz:
-                # no need to generate any more
-                break
-        predictions = torch.cat(predictions, 1)
-        return Output(self.v2t(predictions))
+    def load(self, path):
+        """Return opt and model states."""
+        states = torch.load(path, map_location=lambda cpu, _: cpu)
+        # set loaded states if applicable
+        if self.multigpu:
+            self.model.module.load_state_dict(states['model'])
+        else:
+            self.model.load_state_dict(states['model'])
+        if 'longest_label' in states:
+            self.model.longest_label = states['longest_label']
+        return states
